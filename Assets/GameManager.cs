a239d99af3f99d614d0ffed4d7194f6c15eb620f
KeatonGithub <151184430+KeatonGithub@users.noreply.github.com>
2024-10-24 18:17:39 -0400

    Made Timer and Lives Counter Work

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,16 +6,16 @@ using TMPro;
 
 public class GameManager : MonoBehaviour //defining all the public variables to use with the gamemanage
 {
-    public Transform Spawnpoint;
+    //public Transform Spawnpoint;
     public int LivesCounter = 3;
     public float TimeCounter = 300;
     public TextMeshProUGUI Timer;
-    public TextMeshProUGUI Money;
+    //public TextMeshProUGUI Money;
     public TextMeshProUGUI Lives;
 	//public TextMeshProUGUI Speed;
-	public GameObject Player;
-    public bool winning;
-    public int Collected = 0;
+	//public GameObject Player;
+    //public bool winning;
+    //public int Collected = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,12 +25,12 @@ public class GameManager : MonoBehaviour //defining all the public variables to 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))//forces player respawn if you get stuck when you press r
-        {
-            RespawnP1();
-        }
+        //if (Input.GetKeyDown(KeyCode.R))//forces player respawn if you get stuck when you press r
+        //{
+         //   RespawnP1();
+       // }
 
-        if ( TimeCounter > 0 && winning == false) //this makes it so that when the timer hits zero you lose the game, it also counts down the timer
+        if ( TimeCounter > 0) //this makes it so that when the timer hits zero you lose the game, it also counts down the timer
         {
             TimeCounter -= Time.deltaTime;
         } 
@@ -40,20 +40,20 @@ public class GameManager : MonoBehaviour //defining all the public variables to 
         }
 
         Timer.text = "" + TimeCounter.ToString("F0");//these strings are whats outputted to the UI elements
-        Money.text = "$" + Collected.ToString();//they are the money timer and lives tmp UI text
+       // Money.text = "$" + Collected.ToString();//they are the money timer and lives tmp UI text
         Lives.text = "" + LivesCounter.ToString();
     }
     public void Collect() //adds 100 dollars when you collect a stack of cash
     {
-        Collected += 100;
+       // Collected += 100;
     }
     public void RespawnP1() //this class causes the player to go to the loss scene when they are out of lives it also the respawns the player if they still have lives left
     {
         LivesCounter -= 1;
-        Player.transform.position=Spawnpoint.transform.position;
-        transform.rotation = Quaternion.Euler(0, 0, 0);
+        //Player.transform.position=Spawnpoint.transform.position;
+       //transform.rotation = Quaternion.Euler(0, 0, 0);
 
-        if (LivesCounter < 0 && winning == false)
+        if (LivesCounter < 0 == false)
         {
             SceneManager.LoadScene("Loss");
         }
